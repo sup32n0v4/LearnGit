cf58e8d3e587345749ed14db61bfd4dbe1403349
s742 <sup32n0v4@gmail.com>
2025-07-14 22:27:43 +0700

    add sys library in main.py
    create input of name
    
    print hell name

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import os
+import sys
+if __name__ == "__main__":
+    print("Python version: ", sys.version)
 print("Path :", os.getcwd())
 print("Hello, World!")
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
