343babdb041e7a048774705ed98e1ac690a95e38
sup32n0v4 <s742.sup32n0v4@gmail.com>
2025-07-17 15:24:34 +0700

    ✏️ Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ if __name__ == "__main__":
 print("Path :", os.getcwd())
 print("Hello, World!")
 name = input("Enter your name: ")
+age = input("How old are you : ")
 print(f"Hello, {name}! Have a nice day!")
+print(f"Your {age} years old")
 print("test emoji 😄 commit")
